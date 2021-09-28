afcc6433631d3814ba6d0e9cad580795ed6e0582
Roman <katyrinroman@mail.ru>
2021-09-28 13:57:17 +0700

    Create menu screen

--- app/src/main/java/com/katyrin/bravedeveloperstest/view/MainActivity.kt
+++ app/src/main/java/com/katyrin/bravedeveloperstest/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.katyrin.bravedeveloperstest
+package com.katyrin.bravedeveloperstest.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.katyrin.bravedeveloperstest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
